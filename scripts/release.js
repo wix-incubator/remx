7419508565d3cfbd82c22ae0459cf35539200af8
Daniel Zlotin <danielzl@wix.com>
2017-01-02 16:50:41 +0200

    try fix release

--- scripts/release.js
+++ scripts/release.js
@@ -1,5 +1,3 @@
-const _ = require('lodash');
-
 if (!process.env.CI) {
   throw new Error(`releasing is only available from Travis CI`);
 }
@@ -15,4 +13,6 @@ if (process.env.TRAVIS_PULL_REQUEST !== 'false') {
 }
 
 const cp = require('child_process');
-console.log(String(cp.execSync(`npm userconfig ./npmrc && npm version patch && npm publish && git push`)));
+const p = require('path');
+const npmrcPath = p.resolve(`${__dirname}/npmrc`);
+console.log(String(cp.execSync(`npm userconfig ${npmrcPath} && npm version patch && npm publish && git push`)));
