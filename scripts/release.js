5a7f7cf8d6035bf7054793e3aba2bc8bfda73fc3
Daniel Zlotin <danielzl@wix.com>
2017-01-02 16:36:58 +0200

    explicit release log

--- scripts/release.js
+++ scripts/release.js
@@ -2,9 +2,15 @@ if (!process.env.CI) {
   throw new Error(`releasing is only available from Travis CI`);
 }
 
-if (process.env.TRAVIS_BRANCH !== 'master' || process.env.TRAVIS_PULL_REQUEST) {
+if (process.env.TRAVIS_BRANCH !== 'master') {
+  console.log(`not publishing on branch ${process.env.TRAVIS_BRANCH}`);
+  return;
+}
+
+if (process.env.TRAVIS_PULL_REQUEST) {
+  console.log(`not publishing as triggered by ${process.env.TRAVIS_PULL_REQUEST}`);
   return;
 }
 
 const cp = require('child_process');
-cp.execSync(`npm userconfig ./npmrc && npm version patch && npm publish && git push`);
+console.log(String(cp.execSync(`npm userconfig ./npmrc && npm version patch && npm publish && git push`)));
