7a1ed8bd0cd17816950fd755e80790dcbb6e0080
Daniel Zlotin <danielzl@wix.com>
2017-01-03 09:59:12 +0200

    try to fix release

--- scripts/release.js
+++ scripts/release.js
@@ -1,16 +1,16 @@
-if (!process.env.CI) {
-  throw new Error(`releasing is only available from Travis CI`);
-}
-
-if (process.env.TRAVIS_BRANCH !== 'master') {
-  console.error(`not publishing on branch ${process.env.TRAVIS_BRANCH}`);
-  return;
-}
-
-if (process.env.TRAVIS_PULL_REQUEST !== 'false') {
-  console.error(`not publishing as triggered by pull request ${process.env.TRAVIS_PULL_REQUEST}`);
-  return;
-}
+//if (!process.env.CI) {
+//  throw new Error(`releasing is only available from Travis CI`);
+//}
+//
+//if (process.env.TRAVIS_BRANCH !== 'master') {
+//  console.error(`not publishing on branch ${process.env.TRAVIS_BRANCH}`);
+//  return;
+//}
+//
+//if (process.env.TRAVIS_PULL_REQUEST !== 'false') {
+//  console.error(`not publishing as triggered by pull request ${process.env.TRAVIS_PULL_REQUEST}`);
+//  return;
+//}
 
 const cp = require('child_process');
 const p = require('path');
@@ -19,8 +19,8 @@ function execSync(cmd) {
   cp.execSync(cmd, {stdio: [0, 1, 2]});
 }
 
-execSync(`git config --global user.email "zlotindaniel@gmail.com"`);
-execSync(`git config --global user.name "Daniel Zlotin"`);
-const npmrcPath = p.resolve(`${__dirname}/npmrc`);
-execSync(`npm config set userconfig "${npmrcPath}"`);
-execSync(`npm version patch && npm publish && git push`);
+//execSync(`git config --global user.email "zlotindaniel@gmail.com"`);
+//execSync(`git config --global user.name "Daniel Zlotin"`);
+const npmrcPath = p.resolve(`${__dirname}/.npmrc`);
+execSync(`cp -rf ${npmrcPath} .`);
+//execSync(`npm version patch && npm publish && git push`);
