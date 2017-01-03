f62eba225f7acd455cc505e7a9180d930fa43adf
Daniel Zlotin <danielzl@wix.com>
2017-01-03 15:32:49 +0200

    fixing release

--- scripts/release.js
+++ scripts/release.js
@@ -1,47 +1,56 @@
-if (!process.env.CI || !process.env.TRAVIS) {
-  throw new Error(`releasing is only available from Travis CI`);
-}
-
-if (process.env.TRAVIS_BRANCH !== 'master') {
-  console.error(`not publishing on branch ${process.env.TRAVIS_BRANCH}`);
-  return;
-}
-
-if (process.env.TRAVIS_PULL_REQUEST !== 'false') {
-  console.error(`not publishing as triggered by pull request ${process.env.TRAVIS_PULL_REQUEST}`);
-  return;
-}
-
 const cp = require('child_process');
 const p = require('path');
 const semver = require('semver');
 
 function execSync(cmd) {
-  cp.execSync(cmd, {stdio: [0, 1, 2]});
+  cp.execSync(cmd, {stdio: ['inherit', 'inherit', 'inherit']});
 }
 
 function execSyncRead(cmd) {
-  cp.execSync(cmd);
+  return String(cp.execSync(cmd, {stdio: ['inherit', 'pipe', 'inherit']})).trim();
 }
 
 function execSyncSilently(cmd) {
   cp.execSync(cmd, {stdio: ['ignore', 'ignore', 'ignore']});
 }
 
-execSync(`git config --global push.default simple`);
-execSyncSilently(`git config --global user.email "zlotindaniel@gmail.com"`);
-execSyncSilently(`git config --global user.name "DanielZlotin"`);
-execSyncSilently(`git remote add deploy "https://DanielZlotin:${process.env.GIT_TOKEN}@github.com/wix/remx.git"`);
+function setupGit() {
+  execSync(`git config --global push.default simple`);
+  execSyncSilently(`git config --global user.email "zlotindaniel@gmail.com"`);
+  execSyncSilently(`git config --global user.name "DanielZlotin"`);
+  execSyncSilently(`git remote add deploy "https://DanielZlotin:${process.env.GIT_TOKEN}@github.com/wix/remx.git"`);
+  execSync(`git checkout master`);
+}
 
-execSync(`git checkout master`);
-execSync(`git reset --hard`);
+function run() { //eslint-disable-line
+  if (!process.env.CI || !process.env.TRAVIS) {
+    throw new Error(`releasing is only available from Travis CI`);
+  }
 
-//const currentVersion = JSON.parse(String(execSyncRead(`npm view remx version -j`)));
-//const newVersion;
+  if (process.env.TRAVIS_BRANCH !== 'master') {
+    console.error(`not publishing on branch ${process.env.TRAVIS_BRANCH}`);
+    return;
+  }
 
-const npmrcPath = p.resolve(`${__dirname}/.npmrc`);
-execSync(`cp -rf ${npmrcPath} .`);
+  if (process.env.TRAVIS_PULL_REQUEST !== 'false') {
+    console.error(`not publishing as triggered by pull request ${process.env.TRAVIS_PULL_REQUEST}`);
+    return;
+  }
 
-execSync(`npm version ${newVersion}`);
-execSyncSilently(`git push deploy`);
-execSync(`npm publish`);
+  setupGit();
+
+  const currentVersion = execSyncRead(`npm view remx version`);
+  console.log(`current version is: ${currentVersion}`);
+  const newVersion = semver.inc(currentVersion, `patch`);
+  console.log(`new version is: ${newVersion}`);
+
+  const npmrcPath = p.resolve(`${__dirname}/.npmrc`);
+  execSync(`cp -rf ${npmrcPath} .`);
+
+  execSync(`npm version ${newVersion} -m ${newVersion} [ci skip]`);
+
+  execSyncSilently(`git push deploy --tags`);
+  execSync(`npm publish`);
+}
+
+run();
