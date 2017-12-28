ea191a839b311d1d4ba9513ec95c49606af949e7
Daniel Zlotin <danielzl@wix.com>
2017-12-28 14:02:55 +0200

    deprecation

--- react-native.js
+++ react-native.js
@@ -1,3 +1,5 @@
+console.warn(`Deprecated warning. Please replace this with "require('remx').connect()" or "import {connect} from 'remx'"`);
+
 module.exports = {
   connect: require('./dist/index').connect
 };
