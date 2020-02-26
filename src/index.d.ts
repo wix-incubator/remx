06da01713bdd6a46d07e5268fb5f558809159913
Andrey Kogut <andriik@wix.com>
2020-02-26 16:50:27 +0200

    Add ts typings

---
+++ src/index.d.ts
@@ -0,0 +1,19 @@
+declare module 'remx' {
+  import * as React from 'react';
+
+  interface HOC<TInner, TOuter> {
+    <P extends TInner>(
+      component: React.ComponentType<P>
+    ): React.ComponentType<Omit<P, keyof TInner> & TOuter>;
+  }
+
+  export function connect<TInner, TOuter>(createProps: (a: TOuter) => TInner): HOC<TInner, TOuter>;
+
+  export function connect<T>(component: React.ComponentType<T>): React.ComponentType<T>;
+
+  export function getters<T extends {}>(g: T): T;
+
+  export function state<T extends {}>(s: T): T;
+
+  export function setters<T extends {}>(s: T): T;
+}
