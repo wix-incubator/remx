7d9cb79dea385081bb933f3e64d0aea5f110b18a
Viktoriia Nosova <viktoriian@wix.com>
2020-12-19 15:24:47 +0200

    Added simple store

--- examples/to-do-example/src/components/ToDoList/ToDoList.js
+++ examples/to-do-example/src/components/ToDoList/ToDoList.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
+import { observer } from 'remx';
+
+import { store } from '../../store/store';
 
 import './ToDoList.css';
 
 import ToDoTask from '../ToDoTask/ToDoTask';
 
-function ToDoList() {
+const ToDoList = (props) => {
 	return (
-		<div className='to-do-list-wrapper'>
-			<ToDoTask text='This is my to-do' />
+		<div>
+			{store.getToDos().map((item, index) => (
+				<ToDoTask key={index} text={item.text} />
+			))}
 		</div>
 	);
-}
+};
 
-export default ToDoList;
+export default observer(ToDoList);
