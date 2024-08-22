babfda22ed3a924c8a753ef442e666bef6367a26
Andrew Kogut <andriik@wix.com>
2024-08-22 11:54:20 +0200

    Fix useConnect dependencies validations

--- src/index.d.ts
+++ src/index.d.ts
@@ -19,5 +19,6 @@ declare module 'remx' {
 
   export function setters<T extends {}>(s: T): T;
 
-  export function useConnect<T extends {}>(remxFunc: (...a: A) => T, dependencies?: unknown[]): T;
+  export function useConnect<T extends {}>(remxFunc: () => T, dependencies?: unknown[]): T;
+  export function useConnect<T extends {}, A extends any[]>(remxFunc: (...a: A) => T, dependencies?: A): T;
 }
