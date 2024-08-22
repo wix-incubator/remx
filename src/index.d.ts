099a88f6c282465d41e518e39281aa3785df074d
Andrew Kogut <andriik@wix.com>
2024-08-22 12:21:06 +0200

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -19,5 +19,5 @@ declare module 'remx' {
 
   export function setters<T extends {}>(s: T): T;
 
-  export function useConnect<T extends {}, A extends [any]>(remxFunc: (...a: A) => T, dependencies?: [...A, ...unknown[]]): T;
+  export function useConnect<T extends {}, A extends [any]>(remxFunc: (...a: A) => T, dependencies?: A): T;
 }
